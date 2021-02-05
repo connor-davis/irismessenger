9a7c92258c297f2cfd55cb87858da987546adaa1
Connor Davis <19422@kloofhigh.co.za>
2021-02-05 16:33:23 +0200

    autocommit-1612535603037

--- screens/join.screen.js
+++ screens/join.screen.js
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { hashText } from '../lib/encryption';
-import { setUserEmail, setUserID } from '../lib/slices/user.slice';
+import { setUserEmail, setUserID, setUserName } from '../lib/slices/user.slice';
 
 let JoinScreen = () => {
     let dispatch = useDispatch();
+    let [name, setName] = useState("");
     let [email, setEmail] = useState("");
 
     return (
         <View style={{ flex: 1 }}>
             <View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <View style={{
+                    width: "90%",
+                    backgroundColor: "rgb(232, 232, 232)",
+                    paddingHorizontal: 20,
+                    paddingVertical: 5,
+                    borderRadius: 20,
+                }}>
+                    <TextInput placeholder="What would you be called?" value={name} onChangeText={(text) => setName(text)} style={{ textAlign: "center", color: "rgb(132,132,132)" }} placeholderTextColor="rgb(132,132,132)"></TextInput>
+                </View>
                 <View style={{
                     width: "90%",
                     backgroundColor: "rgb(232, 232, 232)",
@@ -21,6 +31,7 @@ let JoinScreen = () => {
                     <TextInput placeholder="What is your email?" value={email} onChangeText={(text) => setEmail(text)} style={{ textAlign: "center", color: "rgb(132,132,132)" }} placeholderTextColor="rgb(132,132,132)"></TextInput>
                 </View>
                 <TouchableOpacity onPress={() => {
+                    dispatch(setUserName(name));
                     dispatch(setUserEmail(email));
                     dispatch(setUserID(hashText(email)));
                 }}>
