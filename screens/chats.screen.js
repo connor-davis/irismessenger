f22cd5015899d8d76afd67771affa452b18ee5e3
Connor Davis <19422@kloofhigh.co.za>
2021-02-05 16:16:20 +0200

    autocommit-1612534580066

--- screens/chats.screen.js
+++ screens/chats.screen.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { FlatList, ScrollView, Text, View } from 'react-native';
+import { Button, FlatList, ScrollView, Text, View } from 'react-native';
+import { persistor } from '../lib/store';
 
 let ChatsScreen = () => {
     return (
         <View>
+            <Button title="Clear Store" onPress={() => {
+                persistor.purge();
+            }} />
             <FlatList></FlatList>
         </View>
     );
