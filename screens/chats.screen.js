abc77fcfe2b41e789670d3388054a14a538f5c6f
Connor Davis <19422@kloofhigh.co.za>
2021-02-05 17:01:36 +0200

    autocommit-1612537296064

--- screens/chats.screen.js
+++ screens/chats.screen.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Alert, Button, FlatList, ScrollView, Text, View } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { start } from '../lib/protocol';
 import { setUser } from '../lib/slices/user.slice';
 import { persistor } from '../lib/store';
 
 let ChatsScreen = () => {
     let dispatch = useDispatch();
+
+    useEffect(() => {
+        start();
+    }, []);
+
     return (
         <View>
         </View>
