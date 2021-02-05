7acb4e10beffe42076bbf63a850741bcd8d12111
Connor Davis <19422@kloofhigh.co.za>
2021-02-05 21:14:10 +0200

    autocommit-1612552450659

--- screens/chats.screen.js
+++ screens/chats.screen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { init } from '../lib/protocol';
+import { addPeer, init } from '../lib/protocol';
 import { selectUser } from '../lib/slices/user.slice';
 
 let ChatsScreen = () => {
@@ -14,6 +14,9 @@ let ChatsScreen = () => {
 
     return (
         <View>
+            <Button title="Test Add Peer" onPress={() => {
+                addPeer({ id: user.id, info: { name: user.name, }, peerId: user.id });
+            }}/>
         </View>
     );
 }
