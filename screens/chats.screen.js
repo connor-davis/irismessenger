e2483a4a8b1e3d20f96d13f1777c0e2bb03576d3
Connor Davis <19422@kloofhigh.co.za>
2021-02-05 16:29:07 +0200

    autocommit-1612535347213

--- screens/chats.screen.js
+++ screens/chats.screen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Alert, Button, FlatList, ScrollView, Text, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../lib/slices/user.slice';
 import { persistor } from '../lib/store';
 
 let ChatsScreen = () => {
+    let dispatch = useDispatch();
     return (
         <View>
             <Button title="Clear Store" onPress={() => {
-                Alert.alert("Cleared");
-                persistor.purge();
+                dispatch(setUser({}));
             }} />
         </View>
     );
