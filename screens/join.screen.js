f0b7cf42d39e222c9db0f696f232ad5a0c2e93bd
Connor Davis <19422@kloofhigh.co.za>
2021-02-05 21:34:36 +0200

    autocommit-1612553676164

--- screens/join.screen.js
+++ screens/join.screen.js
@@ -8,55 +8,67 @@ let JoinScreen = () => {
     let dispatch = useDispatch();
     let [name, setName] = useState("");
     let [email, setEmail] = useState("");
+    let [areKeysGenerating, setAreKeysGenerating] = useState(false);
 
     return (
         <View style={{ flex: 1 }}>
             <View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                <View style={{
-                    width: "90%",
-                    backgroundColor: "rgb(232, 232, 232)",
-                    paddingHorizontal: 20,
-                    paddingVertical: 5,
-                    marginBottom: 5,
-                    borderRadius: 20,
-                }}>
-                    <TextInput placeholder="What would you be called?" value={name} onChangeText={(text) => setName(text)} style={{ textAlign: "center", color: "rgb(132,132,132)" }} placeholderTextColor="rgb(132,132,132)"></TextInput>
-                </View>
-                <View style={{
-                    width: "90%",
-                    backgroundColor: "rgb(232, 232, 232)",
-                    paddingHorizontal: 20,
-                    paddingVertical: 5,
-                    borderRadius: 20,
-                }}>
-                    <TextInput placeholder="What is your email?" value={email} onChangeText={(text) => setEmail(text)} style={{ textAlign: "center", color: "rgb(132,132,132)" }} placeholderTextColor="rgb(132,132,132)"></TextInput>
-                </View>
-                <TouchableOpacity onPress={() => {
-                    Alert.alert("", "We are generating your security keys, please be patient while this happens and do not close the app.");
-
-                    let generateKeys = async () => {
-                        let keys = await generateKeyPair();
-                        dispatch(setUserKeys(keys));
-                    }
-                    
-                    generateKeys();
-
-                    dispatch(setUserName(name));
-                    dispatch(setUserEmail(email));
-                    dispatch(setUserID(hashText(email)));
-                }}>
-                    <View style={{
-                        backgroundColor: "rgb(150,150,255)",
-                        paddingHorizontal: 10,
-                        paddingVertical: 10,
-                        borderRadius: 10,
-                        marginVertical: 10,
-                    }}>
-                        <Text style={{
-                            color: "rgb(232,232,232)"
-                        }}>Let's Begin</Text>
+                {areKeysGenerating ? (
+                    <View>
+                        <Text style={{ fontWeight: "bold" }}>Generating Security Keys</Text>
+                        <Text style={{ fontWeight: "300" }}>Please be patient and do not exit the app...</Text>
                     </View>
-                </TouchableOpacity>
+                ) : (
+                        <View>
+                            <View style={{
+                                width: "90%",
+                                backgroundColor: "rgb(232, 232, 232)",
+                                paddingHorizontal: 20,
+                                paddingVertical: 5,
+                                marginBottom: 5,
+                                borderRadius: 20,
+                            }}>
+                                <TextInput placeholder="What would you be called?" value={name} onChangeText={(text) => setName(text)} style={{ textAlign: "center", color: "rgb(132,132,132)" }} placeholderTextColor="rgb(132,132,132)"></TextInput>
+                            </View>
+                            <View style={{
+                                width: "90%",
+                                backgroundColor: "rgb(232, 232, 232)",
+                                paddingHorizontal: 20,
+                                paddingVertical: 5,
+                                borderRadius: 20,
+                            }}>
+                                <TextInput placeholder="What is your email?" value={email} onChangeText={(text) => setEmail(text)} style={{ textAlign: "center", color: "rgb(132,132,132)" }} placeholderTextColor="rgb(132,132,132)"></TextInput>
+                            </View>
+                            <TouchableOpacity onPress={() => {
+                                Alert.alert("", "We are generating your security keys, please be patient while this happens and do not close the app.");
+
+                                let generateKeys = async () => {
+                                    let keys = await generateKeyPair();
+                                    setAreKeysGenerating(false);
+
+                                    dispatch(setUserKeys(keys));
+                                    dispatch(setUserName(name));
+                                    dispatch(setUserEmail(email));
+                                    dispatch(setUserID(hashText(email)));
+                                }
+
+                                setAreKeysGenerating(true);
+                                generateKeys();
+                            }}>
+                                <View style={{
+                                    backgroundColor: "rgb(150,150,255)",
+                                    paddingHorizontal: 10,
+                                    paddingVertical: 10,
+                                    borderRadius: 10,
+                                    marginVertical: 10,
+                                }}>
+                                    <Text style={{
+                                        color: "rgb(232,232,232)"
+                                    }}>Let's Begin</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
             </View>
         </View>
     );
