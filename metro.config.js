a275943fe2846dc914ae09b713ff3cc5d8c725ba
Connor Davis <19422@kloofhigh.co.za>
2021-02-09 15:28:14 +0200

    autocommit-1612877294244

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,4 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-const blacklist = require('metro-config/src/defaults/blacklist');
-
 module.exports = {
-  resolver: {
-    extraNodeModules: require('node-libs-react-native'),
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
