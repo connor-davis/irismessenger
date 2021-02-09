0800c87eb98f228480ac06e89e05e8ac5a8ac287
Connor Davis <19422@kloofhigh.co.za>
2021-02-09 11:42:39 +0200

    autocommit-1612863759051

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -2,9 +2,10 @@ var rn_bridge = require('rn-bridge');
 const Hyperbeam = require('hyperbeam');
 
 const beam = new Hyperbeam('this-test-topic');
+const beamPipe = process.stdin.pipe(beam);
 
 rn_bridge.channel.on('message', (msg) => {
-  process.stdin.pipe(beam).pipe(msg);
+  beamPipe.pipe(msg);
 });
 
 rn_bridge.channel.send("Node was initialized.");
