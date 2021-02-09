018ac2e54e1d0aae15828515ff9e237b5d6053f1
Connor Davis <19422@kloofhigh.co.za>
2021-02-09 17:36:10 +0200

    autocommit-1612884970264

--- index.js
+++ index.js
@@ -4,8 +4,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { init } from './lib/protocol';
-
-init();
 
 AppRegistry.registerComponent(appName, () => App);
