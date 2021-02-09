735c9fa3e5872e4db35da29cd803659a55dc8fc8
Connor Davis <19422@kloofhigh.co.za>
2021-02-09 16:21:42 +0200

    autocommit-1612880502922

--- screens/chats.screen.js
+++ screens/chats.screen.js
@@ -1,52 +1,62 @@
-import React, { useEffect } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { init, send } from '../lib/protocol';
-import { selectUser, setUser } from '../lib/slices/user.slice';
+import React, {useEffect} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {init, send} from '../lib/protocol';
+import {selectUser, setUser} from '../lib/slices/user.slice';
 
 let ChatsScreen = () => {
-    let user = useSelector(selectUser);
-    let dispatch = useDispatch();
+  let user = useSelector(selectUser);
+  let dispatch = useDispatch();
 
-    useEffect(() => {
-        init();
-    }, []);
+  useEffect(() => {
+    init();
+  }, []);
 
-    return (
-        <View style={{ flex: 1, alignItems: "center" }}>
-            <TouchableOpacity onPress={() => {
-                send();
+  return (
+    <View style={{flex: 1, alignItems: 'center'}}>
+      <TouchableOpacity
+        onPress={() => {
+          send();
+        }}>
+        <View
+          style={{
+            backgroundColor: 'rgb(0,0,0)',
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+            borderRadius: 10,
+            marginVertical: 10,
+          }}>
+          <Text
+            style={{
+              color: 'rgb(232,232,232)',
             }}>
-                <View style={{
-                    backgroundColor: "rgb(0,0,0)",
-                    paddingHorizontal: 10,
-                    paddingVertical: 10,
-                    borderRadius: 10,
-                    marginVertical: 10,
-                }}>
-                    <Text style={{
-                        color: "rgb(232,232,232)"
-                    }}>Send Test Beam</Text>
-                </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => {
-                dispatch(setUser({}));
-            }}>
-                <View style={{
-                    backgroundColor: "rgb(0,0,0)",
-                    paddingHorizontal: 10,
-                    paddingVertical: 10,
-                    borderRadius: 10,
-                    marginVertical: 10,
-                }}>
-                    <Text style={{
-                        color: "rgb(232,232,232)"
-                    }}>Clear Storage</Text>
-                </View>
-            </TouchableOpacity>
+            Send Test Beam
+          </Text>
         </View>
-    );
-}
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(setUser({}));
+        }}>
+        <View
+          style={{
+            backgroundColor: 'rgb(0,0,0)',
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+            borderRadius: 10,
+            marginVertical: 10,
+          }}>
+          <Text
+            style={{
+              color: 'rgb(232,232,232)',
+            }}>
+            Clear Storage
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default ChatsScreen;
