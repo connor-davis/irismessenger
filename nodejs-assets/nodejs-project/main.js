7c753487f76e2bdc124ebb178483a420332a3e8b
Connor Davis <19422@kloofhigh.co.za>
2021-02-09 11:47:06 +0200

    Update bridge

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -2,10 +2,9 @@ var rn_bridge = require('rn-bridge');
 const Hyperbeam = require('hyperbeam');
 
 const beam = new Hyperbeam('this-test-topic');
-const beamPipe = process.stdin.pipe(beam);
 
 rn_bridge.channel.on('message', (msg) => {
-  beamPipe.pipe(msg);
+  rn_bridge.channel.send.pipe(beam).pipe(msg);
 });
 
 rn_bridge.channel.send("Node was initialized.");
